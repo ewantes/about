06d97561eff31a6d6f4e576254bdb315a7a692df
ewantes <andrew.chabanov@gmail.com>
2015-12-07 00:09:08 +0200

    test

--- js/main.js
+++ js/main.js
@@ -49,3 +49,139 @@ $(document).ready(function() {
 
  
 });
+//home
+//projects
+        jQuery(document).ready(function () {
+
+            $(function () {
+                $(".visible-information").click(function () {
+                    $(this).toggleClass("active-visible-information");
+                })
+            });
+        });
+        //archive
+        jQuery(document).ready(function () {
+
+            $(function () {
+                $(".year-span").click(function () {
+                    $(this).toggleClass("active-year-span");
+                })
+            });
+        });
+ $(document).ready(function() {
+
+        $('#ddmenu li').hover(function() {
+            clearTimeout($.data(this, 'timer'));
+            $('ul', this).stop(true, true).slideDown(0);
+        }, function() {
+            $.data(this, 'timer', setTimeout($.proxy(function() {
+                $('ul', this).stop(true, true).slideUp(0);
+            }, this), 100));
+        });
+
+    });
+ function up() {
+        var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+        if (top > 0) {
+            window.scrollBy(0, ((top + 100) / -10));
+            t = setTimeout('up()', 30);
+        } else clearTimeout(t);
+        return false;
+    }
+    //search
+    jQuery(document).ready(function() {
+
+        $(function() {
+            $(".search").click(function() {
+                $(this).toggleClass('search-opened');
+                $(".search-active").toggleClass("active-search-toggle");
+                $(".input-search").focus();
+            })
+
+            $(".close-search-new").click(function() {
+                $(".search-active").removeClass("active-search-toggle");
+            })
+        });
+    });
+    //search
+    jQuery(document).ready(function() {
+
+        $(function() {
+            $(".cross-search").click(function() {
+                $(".search-active").toggleClass("active-search-toggle");
+            })
+        });
+    });
+    // maps
+    var map;
+    var egglabs = new google.maps.LatLng(50.4523817, 30.5264717);
+    var mapCoordinates = new google.maps.LatLng(50.4523817, 30.5264717);
+
+
+    var markers = [];
+    var image = new google.maps.MarkerImage(
+        'img/maps.png',
+        new google.maps.Size(60, 80),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(42, 56)
+    );
+
+    function addMarker() {
+        markers.push(new google.maps.Marker({
+            position: egglabs,
+            raiseOnDrag: false,
+            icon: image,
+            map: map,
+            draggable: false
+        }));
+
+
+    }
+
+
+    function initialize() {
+        var mapOptions = {
+            backgroundColor: "#ffffff",
+            zoom: 17,
+            disableDefaultUI: true,
+            center: mapCoordinates,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+
+
+
+        };
+
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        addMarker();
+
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+  // select-box
+  $("select").selectBoxIt({
+   theme: "bootstrap"
+
+});
+$("select").bind({
+
+    // Binds to the 'open' event on the original select box
+    "open": function() {
+
+      // Adds the Twitter Bootstrap 'dropup' class to the drop down
+      $(this).data("selectBox-selectBoxIt").dropdown.addClass("dropup");
+
+    },
+
+    // Binds to the 'close' event on the original select box
+    "close": function() {
+
+      // Removes the Twitter Bootstrap 'dropup' class from the drop down
+      $(this).data("selectBox-selectBoxIt").dropdown.removeClass("dropup");
+
+    }
+
+  });
+
+//home
---
+++ js/team.js
@@ -0,0 +1,281 @@
+
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-0").click(function() {
+             $("div.hexagon:has(.people-0)").toggleClass("active-hexagon");
+              $("div#people0").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-1").click(function() {
+             $("div.hexagon:has(.people-1)").toggleClass("active-hexagon");
+              $("div#people1").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-2").click(function() {
+             $("div.hexagon:has(.people-2)").toggleClass("active-hexagon");
+              $("div#people2").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-3").click(function() {
+             $("div.hexagon:has(.people-3)").toggleClass("active-hexagon");
+              $("div#people3").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-4").click(function() {
+             $("div.hexagon:has(.people-4)").toggleClass("active-hexagon");
+              $("div#people4").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-5").click(function() {
+             $("div.hexagon:has(.people-5)").toggleClass("active-hexagon");
+              $("div#people5").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-6").click(function() {
+             $("div.hexagon:has(.people-6)").toggleClass("active-hexagon");
+              $("div#people6").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-7").click(function() {
+             $("div.hexagon:has(.people-7)").toggleClass("active-hexagon");
+              $("div#people7").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-8").click(function() {
+             $("div.hexagon:has(.people-8)").toggleClass("active-hexagon");
+              $("div#people8").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-9").click(function() {
+             $("div.hexagon:has(.people-9)").toggleClass("active-hexagon");
+              $("div#people9").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-10").click(function() {
+             $("div.hexagon:has(.people-10)").toggleClass("active-hexagon");
+              $("div#people10").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-11").click(function() {
+             $("div.hexagon:has(.people-11)").toggleClass("active-hexagon");
+              $("div#people11").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-12").click(function() {
+             $("div.hexagon:has(.people-12)").toggleClass("active-hexagon");
+              $("div#people12").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-13").click(function() {
+             $("div.hexagon:has(.people-13)").toggleClass("active-hexagon");
+              $("div#people13").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-14").click(function() {
+             $("div.hexagon:has(.people-14)").toggleClass("active-hexagon");
+              $("div#people14").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-15").click(function() {
+             $("div.hexagon:has(.people-15)").toggleClass("active-hexagon");
+              $("div#people15").toggleClass("not-active");
+      
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-16").click(function() {
+             $("div.hexagon:has(.people-16)").toggleClass("active-hexagon");
+              $("div#people16").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-17").click(function() {
+             $("div.hexagon:has(.people-17)").toggleClass("active-hexagon");
+              $("div#people17").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-18").click(function() {
+             $("div.hexagon:has(.people-18)").toggleClass("active-hexagon");
+              $("div#people18").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
+    
+      jQuery(document).ready(function(){
+                                     
+             $(function() {   
+             $(".people-19").click(function() {
+             $("div.hexagon:has(.people-19)").toggleClass("active-hexagon");
+              $("div#people19").toggleClass("not-active");
+                 })
+      
+             });
+             });
+      
+      
+    
